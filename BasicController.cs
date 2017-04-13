a893ed2cc14c0b51f8bfb2dc70f8130a435c81e8
ironhall <ca11iditas700@gmail.com>
2017-04-12 23:13:31 -0700

    Original commit

---
+++ BasicController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour
+{
+	void Update ()
+	{
+		Debug.Log("Horizontal Input = " + Input.GetAxis("Horizontal"));
+	}
+}
